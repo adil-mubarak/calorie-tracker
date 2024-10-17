2de388969cbe64a100f8e8bf8220733f7f21c478
adil <aadilmubarake@gmail.com>
2024-10-17 09:58:15 +0530

    second commit

--- SERVER/routes/connection.go
+++ SERVER/routes/connection.go
@@ -1,1 +1,36 @@
 package routes
+
+import (
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DBinstance() *gorm.DB {
+	dsn := "root:kl18jda183079@tcp(localhost:3306)/calorie_tracker?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil{
+		log.Fatal("Failed to conect to the database: ",err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil{
+		log.Fatal("Failed to get DB instance: ",err)
+	}
+	err = sqlDB.Ping()
+	if err != nil{
+		log.Fatal("Failed to ping the database: ",err)
+	}
+
+	fmt.Println("Connected to MySQL database")
+	return db
+}
+
+func OpenCollection(db *gorm.DB,tableName string)*gorm.DB{
+	return db.Table(tableName)
+}
--- SERVER/routes/entries.go
+++ SERVER/routes/entries.go
@@ -46,19 +46,70 @@ func GetEntries(c *gin.Context) {
 }
 
 func GetEntriesByIngredient(c *gin.Context) {
+    ingredient := c.Params.ByName("ingredient")
+    var entries []models.Entry
 
+    if err := db.Where("ingredients LIKE ?","%"+ingredient+"%").Find(&entries).Error; err != nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+        fmt.Println(err)
+        return
+    }
+
+    c.JSON(http.StatusOK,entries)
 }
 
 func GetEntryById(c *gin.Context) {
+    entryID := c.Params.ByName("id")
+    var entry models.Entry
 
+    if err := db.First(&entry,entryID).Error; err != nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"error":"Entry not found"})
+        fmt.Println(err)
+        return
+    }
+
+    c.JSON(http.StatusOK,entry)
 }
 
 func UpdateIngredient(c *gin.Context) {
+    entryID := c.Params.ByName("id")
+    var ingredient struct{
+        Ingredient *string `json:"ingredients"`
+    }
+
+    if err := c.BindJSON(&ingredient); err != nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+        fmt.Println(err)
+        return
+    }
+
+    if err := db.Model(&models.Entry{}).Where("id = ?",entryID).Update("ingredients",ingredient.Ingredient).Error; err != nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"error":"Could not update ingredients"})
+        fmt.Println(err)
+        return
+    }
+
+    c.JSON(http.StatusOK,gin.H{"message":"Ingredients updated successfully"})
 
 }
 
 func UpdateEntry(c *gin.Context) {
+    entryID := c.Params.ByName("id")
+    var entry models.Entry
 
+    if err := c.BindJSON(&entry); err != nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"error":err.Error()})
+        fmt.Println(err)
+        return
+    }
+
+    if err := db.Model(&entry).Where("id = ?",entryID).Updates(entry).Error; err != nil{
+        c.JSON(http.StatusInternalServerError,gin.H{"error":"Could not update the entrie"})
+        fmt.Println(err)
+        return
+    }
+
+    c.JSON(http.StatusOK,entry)
 }
 
 func DeleteEntry(c *gin.Context) {
